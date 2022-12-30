c99a0bd4e9ee45f76222b30f67dbaae6abb05f50
wafa ben miloud <wafabenmiloud@gmail.com>
2022-12-30 16:41:42 +0100

    ok

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -47,7 +47,7 @@ const Contact = () => {
             <input type="text" name='name' placeholder='Your full name' required />
             <input type="email" name='email' placeholder='example@gmail.com' required />
             <textarea name="message" rows="7" placeholder='Your message' required></textarea>
-            <button type='submit' className='btn'>Send Message</button>
+            <button type='submit' className='btnn'>Send Message</button>
           </form>
       </div>
     </section>
